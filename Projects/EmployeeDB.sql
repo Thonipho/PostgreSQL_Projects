4aae2f9633271c6ac33bf937801632fa2d0f68c4
Thonipho <thonipho@icloud.com>
2021-04-21 13:48:34 +0200

    commit message

--- Projects/EmployeeDB.sql
+++ Projects/EmployeeDB.sql
@@ -5,10 +5,10 @@ CREATE TABLE employees (
 	gender varchar,
 	address varchar,
 	email varchar,
-	depart_id int,
-	role_id int,
-	salary_id int,
-	overtime_id int,	
+	depart_id int REFERENCES department(depart_id),
+	role_id int REFERENCES roles(role_id) ,
+	salary_id int REFERENCES salaries(salary_id),
+	overtime_id int REFERENCES overtime(overtime_id),	
 	CONSTRAINT emp_key PRIMARY KEY (emp_id)
 );
 
@@ -59,7 +59,7 @@ VALUES (60000),
 INSERT INTO overtime(overtime_hours,bonus)
 VALUES ('01:00',5000),('02:00',7000),('03:00',10000);
 
-SELECT * FROM overtime
+SELECT * FROM employees
 
 INSERT INTO employees(first_name,surname,gender,address,email,depart_id,role_id,salary_id,overtime_id)
 VALUES ('Thoni', 'Mav', 'Male', 'Midrand,Gauteng', 'thoni@gmail.com', 1, 1, 1, 2 ),
@@ -69,10 +69,10 @@ VALUES ('Thoni', 'Mav', 'Male', 'Midrand,Gauteng', 'thoni@gmail.com', 1, 1, 1, 2
 ('Ora', 'Tilwe', 'Male', 'Zika, Kimberly', 'ora@hotmail.com', 4, 3, 2, 1 ),
 ('Greg', 'Floyd', 'Male', 'Victoria, Western Cape', 'gred@hotmail.com', 2, 1, 1, 1 ),
 ('Lesedi', 'Libe', 'Female', 'Phuma, Durban', 'libe@hotmail.com', 3, 2, 2, 1 ),
-('Champ', 'Jonga', 'Male', 'Oliven, Gauteng', 'champ@gmail.com', 1, 3, 3, 1 ),
+('Champ', 'Jonga', 'Male', 'Oliven, Gauteng', 'champ@gmail.com', 1, 3, 3, 3 ),
 ('Pretty', 'Ontha', 'Female', 'Malibu, Kimberly', 'ontha@hotmail.com', 4, 2, 1, 2 ),
 ('Fred', 'Kal', 'Male', 'Midland, Western Cape', 'fred@hotmail.com', 2, 2, 2, 1 ),
-('Del', 'Piero', 'Male', 'Joburg, Gauteng', 'del@hotmail.com', 1, 4, 4, 1 ),
+('Del', 'Piero', 'Male', 'Joburg, Gauteng', 'del@hotmail.com', 1, 4, 4, 2 ),
 ('Mnqobi', 'Buze', 'Male', 'Pineville, Durban', 'buze@hotmail.com', 3, 3, 3, 1 ),
 ('Parresen', 'Milner', 'Male', 'Lansa, Western Cape', 'milner@gmail.com', 2, 3, 3, 2 ),
 ('Ofentse', 'Mad', 'Female', 'Sandton, Gauteng', 'ofentse@icloud.com', 1, 2, 2, 3 ),
@@ -84,3 +84,4 @@ from employees as em left join department as dm on em.depart_id = dm.depart_id
 left join roles as rl on em.role_id = rl.role_id
 left join salaries as sl on em.salary_id = sl.salary_id
 left join overtime as ot on em.overtime_id = ot.overtime_id
+
--- TryItYourself/tryityourself_queries.sql
+++ TryItYourself/tryityourself_queries.sql
@@ -1,22 +1,22 @@
 --tryityourself page 45
 
-CREATE TABLE animal_kind (
-kind varchar,
-weight numeric);
+1. CREATE TABLE animal_kind (
+   kind varchar,
+   weight numeric);
 
-CREATE TABLE animal_species(
-habitat varchar,
-food varchar)
+   CREATE TABLE animal_species(
+   habitat varchar,
+   food varchar)
 
-INSERT INTO animal_kind (kind, weight)
-VALUES ('dog', 111),
-('lion', 232),
-('elephant', 433);
+2. INSERT INTO animal_kind (kind, weight)
+   VALUES ('dog', 111),
+   ('lion', 232),
+   ('elephant', 433);
 
-INSERT INTO animal_species(habitat, food)
-VALUES ('domestic', 'omnivore'),
-('safari', 'carnivore'),
-('safari', 'herbivore');
+   INSERT INTO animal_species(habitat, food)
+   VALUES ('domestic', 'omnivore'),
+   ('safari', 'carnivore'),
+   ('safari', 'herbivore');
 
 SELECT * FROM animal_kind
 
@@ -24,17 +24,58 @@ SELECT * FROM animal_species
 
 --tryityourself page 57
 
-SELECT first_name
-FROM teachers
-u WHERE first_name LIKE 'sam%';
+1. SELECT last_name, school
+   FROM teachers
+   ORDER BY last_name;
 
-SELECT first_name
-FROM teachers
-u WHERE first_name LIKE 'sam%';
+2. SELECT first_name, last_name
+   FROM teachers
+   WHERE first_name LIKE 's%' AND salary>40000;
 
-SELECT first_name
-FROM teachers
-u WHERE first_name LIKE 'sam%';
+3. SELECT first_name, last_name
+   FROM teachers
+   WHERE hire_date>'2010-01-01'
+   ORDER BY salary DESC;
+
+--tryityourself page 72
+
+--1. I would use numeric or decimal because miles have decimal points 
+--2. Varchar would be the correct data type to use. Seperating first name and last name makes the data more accurate and it saves storage.
+--3. You will get an error
+
+--tryityourself page 89
+
+1. CREATE TABLE movies (id integer,
+   movie varchar, actor varchar);
+   
+   COPY movies 
+   FROM 'C:\Users\User\Desktop\Code College\PostgreSQL/Analysis/movies.csv'
+   WITH (FORMAT CSV, HEADER)
+
+2.COPY (
+  SELECT geo_name, housing_unit_count_100_percent, geo_name
+  FROM us_counties_2010
+  LIMIT 20
+  )
+  TO 'C:\Users\User\Desktop\Code College\PostgreSQL/Analysis\housing_units.txt'
+  WITH (FORMAT CSV, HEADER, DELIMITER '|');
+  
+3. It will operate
+
+--tryityourself page 106
+1. SELECT PI()*5^2 AS "Circle area"
+
+2. SELECT geo_name,(CAST (p0010004 AS numeric(8,1)) / p0010001) * 100 AS "American
+   Indian/Alaska Native Alone"
+	FROM us_counties_2010
+	ORDER BY "American
+   Indian/Alaska Native Alone" DESC;
+	
+3. --yes
+
+EXPLAIN ANALYZE SELECT * FROM animal_kind
+WHERE kind = 'lion';
+   
 
 
 
